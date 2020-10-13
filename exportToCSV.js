d6828f0118b6bce5fc5069794616cfeb4f76939f
Solijon Sharipov <40540002+Solijons@users.noreply.github.com>
2020-10-13 13:22:58 -1000

    Updated export csv function

--- exportToCSV.js
+++ exportToCSV.js
@@ -1,19 +1,27 @@
-  function handleExport() {
-    const url = `${API_URL}/temperatures/${requestedValToExport.site}/${dateFormat(requestedValToExport.startDate)}/${dateFormat(requestedValToExport.endDate)}/csvfile`;
-    getToken()
-      .then((token) => {
-        fetch(url, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-          .then((res) => res.blob())
-          .then((blob) => {
-            const a = document.createElement('a');
-            a.href = window.URL.createObjectURL(blob);
-            a.download = ('temperatures.csv');
-            document.body.appendChild(a);
-            a.click();
-          });
-      });
+const exportCsv = (rowData, name) => {
+  const anchor = document.createElement('a');
+  const csvRows = [];
+
+  let csvString = '';
+  let headers = '';
+
+  for (const row of rowData) {
+    // tslint:disable: object-literal-sort-keys
+    csvRows.push({
+      // row data i.e row.id
+      id: row.id,
+    });
   }
+
+  headers = Object.keys(csvRows[0]).map((header) => `"${header}"`).join(',') + '\n';
+
+  csvString += headers;
+  csvRows.forEach((row) => csvString += Object.values(row).map((val) => `"${val}"`).join(',') + '\n');
+
+  anchor.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvString));
+  anchor.setAttribute('download', `${name}.csv`);
+  anchor.style.display = 'none';
+  document.body.appendChild(anchor);
+  anchor.click();
+  document.body.removeChild(anchor);
+};
